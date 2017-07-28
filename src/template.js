3f8222c13184ca2af43d81a1e53c789bf815787c
Dane Pilcher <danepilch@gmail.com>
2017-07-28 14:30:56 -0400

    output functions

--- src/template.js
+++ src/template.js
@@ -3,24 +3,23 @@ import doT from 'dot';
 const template = `
 # {{=it.name}}
 
-{{~it.abiDocs :docItem:index}}
-## {{=docItem.name}}
-
-{{=docItem.notice}}
-
-{{=docItem.details}}
-
-Inputs
+{{~it.abiDocs :docItem:index}}{{? docItem.type === 'function'}}
+## {{=docItem.type}} {{=docItem.name}} \`{{=docItem.signatureHash}}\`
+{{?docItem.notice}}
+**{{=docItem.notice}}**
+{{?}}
+{{?docItem.details}}> {{=docItem.details}}
+{{?}}
+{{? docItem.inputs.length > 0 }}Inputs
 | | | |
 |-|-|-|{{~docItem.inputs :input}}
-| {{=input.type}} | {{=input.name}} | {{=input.description}} |{{~}}
-
-Outputs
+| *{{=input.type}}* | {{=input.name}} | {{=input.description}} |{{~}}{{?}}
+{{? docItem.outputs.length > 0 }}Outputs
 | | | |
 |-|-|-|{{~docItem.outputs :output}}
-| {{=output.type}} | {{=output.name}} | {{=output.description}} |{{~}}
+| {{=output.type}} | {{=output.name}} | {{=output.description}} |{{~}}{{?}}{{?}}
 {{~}}
-`;
+---`;
 
 doT.templateSettings.strip = false;
 
