aec9e623ec64288daa8b79da149ef5dd9acaa5a2
Dane Pilcher <danepilch@gmail.com>
2017-07-28 15:02:45 -0400

    add navigation

--- src/markdown.js
+++ src/markdown.js
@@ -6,6 +6,15 @@ export default function ({ args, data }) {
   const writeStream = fs.createWriteStream(args.dest, { flags: 'w' });
   return new Promise((resolve) => {
     data.forEach((contract) => {
+      writeStream.write(`* [${contract.name}](#${contract.name.toLowerCase()})\n`);
+      contract.abiDocs.forEach((docItem) => {
+        if (typeof docItem.name !== 'undefined') {
+          writeStream.write(`  * [${docItem.name}](#${docItem.type}-${docItem.name.toLowerCase()})\n`);
+        }
+      });
+    });
+
+    data.forEach((contract) => {
       const md = template(contract);
       writeStream.write(md);
     });
--- src/template.js
+++ src/template.js
@@ -5,17 +5,22 @@ const template = `
 {{? it.author }}
 {{=it.author}}{{?}}
 {{~it.abiDocs :docItem:index}}{{? docItem.type === 'function'}}
-## {{=docItem.type}} {{=docItem.name}} \`{{=docItem.signatureHash}}\`
+## {{=docItem.type}} {{=docItem.name}}
+
+\`{{=docItem.signatureHash}}\` {{?docItem.payable}}\`payable\`
+{{?}}
 {{?docItem.notice}}
 **{{=docItem.notice}}**
 {{?}}
 {{?docItem.details}}> {{=docItem.details}}
 {{?}}
 {{? docItem.inputs.length > 0 }}Inputs
+
 | | | |
 |-|-|-|{{~docItem.inputs :input}}
 | *{{=input.type}}* | {{=input.name}} | {{=input.description}} |{{~}}{{?}}
 {{? docItem.outputs.length > 0 }}Outputs
+
 | | | |
 |-|-|-|{{~docItem.outputs :output}}
 | {{=output.type}} | {{=output.name}} | {{=output.description}} |{{~}}{{?}}{{?}}
