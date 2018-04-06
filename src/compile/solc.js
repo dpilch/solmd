bb825bff3874565f75b7c0c4663a36a976184f40
Jiří Chadima <chadima.jiri@gmail.com>
2018-04-06 14:26:22 +0200

    feature: Replaces system-wide solc with solc from npm (#13)
    Does not recompile template.js as there is an issue discussed in #10

--- src/compile/solc.js
+++ src/compile/solc.js
@@ -1,25 +1,57 @@
-import childProcess from 'child_process';
+import fs from 'fs';
+import solc from 'solc';
+
+function findImports(path) {
+  const output = fs.readFileSync(path.replace('file://', ''));
+  return { contents: output.toString() };
+}
 
 export default function (src) {
   return new Promise((resolve) => {
-    const exec = `solc --combined-json abi,asm,ast,bin,bin-runtime,clone-bin,devdoc,interface,opcodes,srcmap,srcmap-runtime,userdoc ${src}`;
-    const rawRes = childProcess.execSync(exec);
-    const res = JSON.parse(rawRes);
+    const sources = {};
+    sources[src] = {
+      urls: [`file://${src}`],
+    };
+    const output = solc.compileStandardWrapper(JSON.stringify({
+      language: 'Solidity',
+      sources,
+      settings: {
+        outputSelection: {
+          '*': {
+            '*': [
+              'abi',
+              'asm',
+              'ast',
+              'bin',
+              'bin-runtime',
+              'clone-bin',
+              'interface',
+              'opcodes',
+              'srcmap',
+              'srcmap-runtime',
+              'devdoc',
+              'userdoc',
+            ],
+          },
+        },
+      },
+    }), findImports);
+    const res = JSON.parse(output);
     resolve({
       contracts: Object.keys(res.contracts).reduce((o, k) => {
         const file = k.split(':')[0];
         const fileFragments = file.split('/');
         const contractName = fileFragments[fileFragments.length - 1].split('.sol')[0];
-        const contract = res.contracts[k];
+        const contract = res.contracts[k][contractName];
         const fileName = `${process.env.PWD}/${k.split(':')[0]}`;
         return {
           ...o,
           [contractName]: {
             ...contract,
             fileName,
-            abi: JSON.parse(contract.abi),
-            devdoc: JSON.parse(contract.devdoc),
-            userdoc: JSON.parse(contract.userdoc),
+            abi: contract.abi,
+            devdoc: contract.devdoc,
+            userdoc: contract.userdoc,
           },
         };
       }, {}),
