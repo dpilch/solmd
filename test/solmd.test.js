fa82205e84b99157d4a610c5ca3529b070792eaa
Dane Pilcher <danepilch@gmail.com>
2018-04-06 15:38:44 -0400

    reorg tests

--- test/solmd.test.js
+++ test/solmd.test.js
@@ -3,11 +3,11 @@ const assert = require('assert');
 const minimist = require('minimist');
 const fs = require('fs');
 const Solmd = require('../lib');
-const expected = require('./expected');
+const expected = require('./solmd/expected');
 
 describe('solmd', () => {
   it('GavCoin', () => {
-    const args = minimist(['test/general/GavCoin.sol', '--dest', 'test/output.md']);
+    const args = minimist(['test/solmd/general/GavCoin.sol', '--dest', 'test/output.md']);
     return Solmd.default.build(args)
       .then(() => fs.readFileSync('./test/output.md', 'utf8'))
       .then((res) => {
@@ -16,7 +16,7 @@ describe('solmd', () => {
   });
 
   it('should have no ToC', () => {
-    const args = minimist(['test/notoc/GavCoin.sol', '--dest', 'test/output.md', '--notoc']);
+    const args = minimist(['test/solmd/notoc/GavCoin.sol', '--dest', 'test/output.md', '--notoc']);
     return Solmd.default.build(args)
       .then(() => fs.readFileSync('./test/output.md', 'utf8'))
       .then((res) => {
@@ -25,7 +25,7 @@ describe('solmd', () => {
   });
 
   it('should have events', () => {
-    const args = minimist(['test/events/GavCoin.sol', '--dest', 'test/output.md']);
+    const args = minimist(['test/solmd/events/GavCoin.sol', '--dest', 'test/output.md']);
     return Solmd.default.build(args)
       .then(() => fs.readFileSync('./test/output.md', 'utf8'))
       .then((res) => {
